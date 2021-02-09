0109b88d883d91b43cfb68ac0a192ab0add224ee
zulqarnain <zulqarnain@datics.ai>
2021-02-09 17:26:07 +0500

    deleted everything

--- app/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const RESTART_ON_REMOUNT = '@@saga-injector/restart-on-remount';
-export const DAEMON = '@@saga-injector/daemon';
-export const ONCE_TILL_UNMOUNT = '@@saga-injector/once-till-unmount';
-
-export const style_constants = {
-  headerWidth: '5rem',
-  menuWidth: '5rem'
-};
