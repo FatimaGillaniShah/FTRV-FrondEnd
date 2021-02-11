9769ced7ee94856d636b267fa51bf4dad2deec52
zulqarnain <zulqarnain@datics.ai>
2021-02-12 00:51:08 +0500

    Cleaned Scaffolding and added basic Material UI Components , cleaned package.json file

--- app/app.js
+++ app/app.js
@@ -11,8 +11,6 @@ import '@babel/polyfill';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 // Import root app
 import App from 'containers/App';
-// import 'file-loader?name=.htaccess!./.htaccess'; // eslint-disable-line import/extensions
-import FontFaceObserver from 'fontfaceobserver';
 // Import all the third party stuff
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -22,13 +20,6 @@ import MUI_Theme from './theme';
 
 // Observe loading of Open Sans (to remove open sans, remove the <link> tag in
 // the index.html file and this observer)
-const openSansObserver = new FontFaceObserver('Open Sans', {});
-
-// When Open Sans is loaded, add a font-family using Open Sans to the body
-openSansObserver.load().then(() => {
-  document.body.classList.add('fontLoaded');
-});
-
 const MOUNT_NODE = document.getElementById('app');
 
 (function render() {
