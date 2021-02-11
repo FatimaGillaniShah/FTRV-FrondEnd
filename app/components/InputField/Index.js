8938d8d79280227b9d0490da2760f20c030bf57a
Hamza <ali.hamza@datics.ai>
2021-02-11 14:12:25 +0500

    added proptypes and more flexibility regarding icon

--- app/components/InputField/Index.js
+++ app/components/InputField/Index.js
@@ -10,6 +10,7 @@ import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControl from '@material-ui/core/FormControl';
+import PropTypes from 'prop-types';
 
 export default function InputField(props) {
   const {
@@ -18,27 +19,58 @@ export default function InputField(props) {
     inputType,
     inputID,
     onIconClick,
-    iconID
+    iconID,
+    appendIcon,
+    prependIcon,
+    fullWidth
   } = props;
-
+  console.log(Icon, 'icon');
   return (
-    <FormControl>
+    <FormControl fullWidth={fullWidth}>
       <InputLabel htmlFor={inputID}>{placeholderText}</InputLabel>
       <Input
         id={inputID}
         type={inputType}
         {...props}
         endAdornment={
-          <InputAdornment position="start">
-            <IconButton id={iconID} onClick={onIconClick} {...props}>
-              {Icon && <Icon />}
-            </IconButton>
-          </InputAdornment>
+          Icon &&
+          appendIcon && (
+            <InputAdornment position="end">
+              <IconButton id={iconID} onClick={onIconClick} {...props}>
+                <Icon />
+              </IconButton>
+            </InputAdornment>
+          )
+        }
+        startAdornment={
+          Icon &&
+          prependIcon && (
+            <InputAdornment position="start">
+              <IconButton id={iconID} onClick={onIconClick} {...props}>
+                <Icon />
+              </IconButton>
+            </InputAdornment>
+          )
         }
       />
     </FormControl>
   );
 }
+InputField.propTypes = {
+  fullWidth: PropTypes.bool,
+  placeholderText: PropTypes.string,
+  Icon: PropTypes.object,
+  inputType: PropTypes.string,
+  inputID: PropTypes.string,
+  onIconClick: PropTypes.func,
+  iconID: PropTypes.string,
+  appendIcon: PropTypes.bool,
+  prependIcon: PropTypes.bool,
+  fullWidth: PropTypes.bool
+};
+InputField.defaultProps = {
+  fullWidth: true
+};
 
 // Usage
 
@@ -50,5 +82,9 @@ export default function InputField(props) {
   inputID="abc"
   onIconClick={handleChange}
   iconID="ad"
+  Icon={EmailIcon}
+  placeholderText="Email"
+  appendIcon={true}
+  prependIcon={false}   
   ...otherProps
 />; */
