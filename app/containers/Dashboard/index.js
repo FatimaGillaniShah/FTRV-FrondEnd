7a0044e41becc56f8307cd803676c998fa78c9b4
Hamza <ali.hamza@datics.ai>
2021-02-11 14:50:13 +0500

    added start and endadnormant

--- app/containers/Dashboard/index.js
+++ app/containers/Dashboard/index.js
@@ -7,8 +7,6 @@
 import React, { memo } from 'react';
 import { Helmet } from 'react-helmet';
 import DashboardComponent from 'components/DashboardComponent/Loadable';
-import InputField from '../../components/InputField/Index';
-import EmailIcon from '@material-ui/icons/Email';
 function Dashboard({ Children }) {
   return (
     <>
@@ -16,10 +14,7 @@ function Dashboard({ Children }) {
         <title>Dashboard</title>
         <meta name="description" content="Description of Dashboard" />
       </Helmet>
-      {/* <DashboardComponent Children={Children} /> */}
-      <div style={{ margin: '100px 100px' }}>
-        <InputField variant="outlined" prependIcon={true} Icon={EmailIcon} />
-      </div>
+      <DashboardComponent Children={Children} />
     </>
   );
 }
