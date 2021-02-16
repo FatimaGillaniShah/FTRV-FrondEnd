52aa06f3e2c2f13c58d077c180222d6cb48f50f1
Hamza <ali.hamza@datics.ai>
2021-02-16 11:38:11 +0500

    loginPage design

---
+++ app/components/LoginPage/Index.js
@@ -0,0 +1,70 @@
+/**
+ *
+ * Login
+ *
+ */
+
+import React, { memo } from 'react';
+import { Helmet } from 'react-helmet';
+import Container from '@material-ui/core/Container';
+import InputField from '../../components/InputField/Index';
+import Box from '@material-ui/core/Box';
+import CustomButton from '../CustomButton/Index';
+import useStyles from './style.js';
+import Paper from '@material-ui/core/Paper';
+import EmailIcon from '@material-ui/icons/Email';
+import LockIcon from '@material-ui/icons/Lock';
+function Login() {
+  const classes = useStyles();
+  return (
+    <>
+      <Helmet>
+        <title>Login</title>
+        <meta name="description" content="Description of Login" />
+      </Helmet>
+      <div className={classes.bgContainer}>
+        <Container component="main" maxWidth="xs">
+          <div className={classes.loginContainer}>
+            <Box className={classes.welcomBox}>
+              <h1>Welcome!</h1>
+            </Box>
+            <Paper className={classes.loginBox}>
+              <Box mt={7}>
+                <InputField
+                  name="email"
+                  placeholderText="Email"
+                  Icon={EmailIcon}
+                  appendIcon={true}
+                  IconClickable={false}
+                />
+              </Box>
+              <Box my={5}>
+                <InputField
+                  name="password"
+                  placeholderText="Password"
+                  Icon={LockIcon}
+                  appendIcon={true}
+                  IconClickable={false}
+                />
+              </Box>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center'
+                }}
+              >
+                <CustomButton
+                  btnText="login"
+                  fullWidth={false}
+                  className={classes.loginBtn}
+                />
+              </div>
+            </Paper>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default memo(Login);
