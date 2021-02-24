3c9c1034d087ac53534206c01f66f281f2943b6b
Hamza <ali.hamza@datics.ai>
2021-02-24 11:39:03 +0500

    pr comments

--- app/components/LoginPage/Index.js
+++ app/components/LoginPage/Index.js
@@ -7,13 +7,13 @@
 import React, { memo } from 'react';
 import { Helmet } from 'react-helmet';
 import Container from '@material-ui/core/Container';
-import InputField from '../../components/InputField/Index';
 import Box from '@material-ui/core/Box';
-import CustomButton from '../CustomButton/Index';
-import useStyles from './style.js';
 import Paper from '@material-ui/core/Paper';
 import EmailIcon from '@material-ui/icons/Email';
 import LockIcon from '@material-ui/icons/Lock';
+import useStyles from './style.js';
+import CustomButton from '../CustomButton/Index';
+import { Input } from '../index';
 function Login() {
   const classes = useStyles();
   return (
@@ -23,27 +23,27 @@ function Login() {
         <meta name="description" content="Description of Login" />
       </Helmet>
       <Box className={classes.bgContainer}>
-        <Container component="main" maxWidth="xs">
+        <Container maxWidth="xs">
           <Box className={classes.loginContainer}>
             <Box className={classes.welcomBox}>
               <h1>Welcome!</h1>
             </Box>
             <Paper className={classes.loginBox}>
               <Box mt={7}>
-                <InputField
+                <Input
                   name="email"
                   placeholderText="Email"
                   Icon={EmailIcon}
-                  appendIcon={true}
+                  appendIcon
                   IconClickable={false}
                 />
               </Box>
               <Box my={5}>
-                <InputField
+                <Input
                   name="password"
                   placeholderText="Password"
                   Icon={LockIcon}
-                  appendIcon={true}
+                  appendIcon
                   IconClickable={false}
                 />
               </Box>
