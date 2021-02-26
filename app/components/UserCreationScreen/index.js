70c96eb0e19ec298df78edc79410ce857efc64b7
Muhammad Zulqarnain Sabir <zulqarnain@datics.ai>
2021-02-26 11:46:25 +0500

    initial file changes

---
+++ app/components/UserCreationScreen/index.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * UserCreationScreen
+ *
+ */
+
+import React, { memo } from 'react';
+// import PropTypes from 'prop-types';
+import { Box, Container, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+//import clsx from 'clsx';
+
+const useStyles = makeStyles((theme) => ({}));
+function UserCreationScreen() {
+  const classes = useStyles();
+  return <></>;
+}
+
+export default memo(UserCreationScreen);
---
+++ app/components/UserCreationScreen/loadable.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * Asynchronously loads the component for UserCreationScreen
+ *
+ */
+
+import loadable from 'utils/loadable';
+
+export default loadable(() => import('./index'));
