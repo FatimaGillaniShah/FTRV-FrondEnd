33a41f6889b995daccbea4c05860ba00476e2054
alihamza-datics <66749557+alihamza-datics@users.noreply.github.com>
2021-03-17 14:36:11 +0500

    reusable birthday card with slider (#39)
    * reusable birthday card with slider added
    
    * removed data and flex
    
    * Delete carouselItems.js

--- app/components/layout/rightInfoPanel/index.js
+++ app/components/layout/rightInfoPanel/index.js
@@ -1,6 +1,7 @@
 import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
+import BirthdayCarousel from '../../birthdayCard';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,17 +14,13 @@ const useStyles = makeStyles((theme) => ({
     marginBlock: '0.2rem',
 
     backgroundColor: theme.palette.bgColor.secondary,
-    flex: '0.25',
   },
   birthdaySection: {
     marginBlock: '0.2rem',
-    backgroundColor: theme.palette.bgColor.secondary,
-    flex: '0.45',
   },
   motivationSection: {
     marginBlock: '0.2rem',
     backgroundColor: theme.palette.bgColor.secondary,
-    flex: '0.3',
   },
 }));
 function Index() {
@@ -33,7 +30,9 @@ function Index() {
       <Grid xs={12} className={classes.root}>
         <Grid xs={12} className={classes.notificationsSection} />
 
-        <Grid xs={12} className={classes.birthdaySection} />
+        <Grid xs={12} className={classes.birthdaySection}>
+          <BirthdayCarousel />
+        </Grid>
 
         <Grid xs={12} className={classes.motivationSection} />
       </Grid>
