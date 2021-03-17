c69ba1083ba9d3412e5f743cbfbf62147ad85521
Fatima Shah <fatimashah5184@gmail.com>
2021-03-17 11:11:24 +0600

    styling

--- app/components/pages/announcementNotification/index.js
+++ app/components/pages/announcementNotification/index.js
@@ -9,19 +9,17 @@ export function AnnouncementNotification() {
   return (
     <Box className={classes.mainBox}>
       <Box width={1 / 4} className={classes.notificationBox}>
-        <Box width={1 / 2}></Box>
-        <Box width={1 / 2} className={classes.iconBox}>
+        <Box className={classes.iconBox}>
           <NotificationsActiveIcon className={classes.icon} />
         </Box>
       </Box>
 
-      <Box width={1 / 2} className={classes.typographyBox}>
+      <Box className={classes.typographyBox}>
         <Box className={classes.titleBox}>
           <Typography className={classes.title}>Notification</Typography>
         </Box>
         <Box>
           <Typography className={classes.description}>
-            {' '}
             The office will be closed.
           </Typography>
         </Box>
