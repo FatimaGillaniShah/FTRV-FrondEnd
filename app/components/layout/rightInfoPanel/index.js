cf285419a0a97a021bc410a3546ad09468b77409
Usman Qamar <usmanqamar189@hotmail.com>
2021-03-26 14:55:21 +0500

    feedback addressed

--- app/components/layout/rightInfoPanel/index.js
+++ app/components/layout/rightInfoPanel/index.js
@@ -8,7 +8,7 @@ import { keys } from '../../../state/queryKeys';
 import { getBirthdays, getQuote } from '../../../state/queryFunctions';
 import BoxWithBg from '../../boxWithBg';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
@@ -16,9 +16,6 @@ const useStyles = makeStyles((theme) => ({
     ' & > div': {
       marginBottom: '1rem',
     },
-  },
-  birthdaySection: {
-    backgroundColor: theme.palette.bgColor.secondary,
   },
   motivationSection: {
     minHeight: 80,
@@ -48,7 +45,7 @@ function Index() {
             <BoxWithBg
               styles={classes.motivationSection}
               title="Daily Dose  of Motivation"
-              bgColor="primary.main"
+              bgColor="secondary.main"
             >
               {quote}
             </BoxWithBg>
