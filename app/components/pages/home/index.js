fe6ef4b8a1de4704eb48326ac653dadeb311dd16
fatimashah5184 <79968121+fatimashah5184@users.noreply.github.com>
2021-04-09 19:01:18 +0600

    banner image added (#79)
    Co-authored-by: fatimashah5184 <fatimashah5184@gmail.com>

--- app/components/pages/home/index.js
+++ app/components/pages/home/index.js
@@ -1,6 +1,7 @@
-import { Box, Grid } from '@material-ui/core';
+import { Avatar, Box, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
+import bannerImage from '../../../images/group.png';
 import { EventCalendar } from '../events/calendar';
 
 const useStyles = makeStyles((theme) => ({
@@ -14,11 +15,20 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.bgColor.secondary,
     flex: '0.3',
     marginBlock: '0.2rem',
+    padding: theme.spacing(6, 20),
   },
   statsSection: {
     backgroundColor: theme.palette.bgColor.secondary,
     flex: '0.7',
     marginBlock: '0.2rem',
+  },
+  bannerImage: {
+    backgroundImage: `url(${bannerImage})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    width: '100%',
+    backgroundPosition: 'center',
+    height: '25vh',
   },
 }));
 function Home({ eventList, isLoading }) {
@@ -27,7 +37,9 @@ function Home({ eventList, isLoading }) {
   return (
     <>
       <Grid xs={12} className={classes.root}>
-        <Grid xs={12} className={classes.bannerGridSection} />
+        <Grid xs={12} className={classes.bannerGridSection}>
+          <Box className={classes.bannerImage} />
+        </Grid>
         <Grid xs={12} className={classes.statsSection}>
           <Box m={10}>
             <Box height="50vh" width={[1, 1, 1, 1 / 2]}>
