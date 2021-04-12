c48489086ecf06426ab717ee7f36a178bd190e5a
fatimashah5184 <fatimshah5184@gmail.com>
2021-04-12 14:47:10 +0600

    msg

--- app/containers/addUsefulLink/index.js
+++ app/containers/addUsefulLink/index.js
@@ -23,7 +23,7 @@ function AddUsefulLink() {
       enabled: !!id,
     }
   );
-  let a;
+
   const mutation = useMutation(id ? updateLink : createLink, {
     onSuccess: () => {
       history.push('/useful-links');
