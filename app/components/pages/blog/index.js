1dd7d766415de9f4297483fdf3e212f7e74bfc0a
fatimashah5184 <fatimashah5184@gmail.com>
2021-04-15 22:32:30 +0600

    props added

--- app/components/pages/blog/index.js
+++ app/components/pages/blog/index.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { memo } from 'react';
 import Box from '@material-ui/core/Box';
 import { Avatar, IconButton } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import { H5, BodyTextLarge, BodyTextSmall } from '../../typography';
-import blog1 from '../../../images/blog1.jpeg';
 import { useAuthContext } from '../../../context/authContext';
 import { ROLES } from '../../../utils/constants';
-import { colors } from '../../../theme/colors';
 
 const useStyles = makeStyles(() => ({
+  main: {
+    margin: 'auto',
+  },
   imageView: {
     width: '80%',
     height: '100%',
   },
-  textColor: {
-    color: colors.grey,
-  },
 }));
 
-export default function Blog() {
+function Blog({ blog }) {
   const {
     user: {
       data: { role },
@@ -34,15 +33,20 @@ export default function Blog() {
         flexDirection={['column', 'column', 'row', 'row']}
         mt={5}
         mb={5}
+        className={classes.main}
       >
         <Box width={[1, 1, 1, '20%']} mt={3}>
           {' '}
-          <Avatar variant="square" src={blog1} className={classes.imageView} />
+          <Avatar
+            variant="square"
+            src={blog.thumbnail}
+            className={classes.imageView}
+          />
         </Box>
         <Box width={[1, '0.78']}>
           <Box display="flex" flexDirection="row">
             <Box width={[1, 1 / 2]} mt={2}>
-              <H5>12 Reasons to Celebrate Life</H5>
+              <H5>{blog.title}</H5>
             </Box>
             {role === ROLES.ADMIN && (
               <Box width={[1, 1 / 2]} display="flex" justifyContent="flex-end">
@@ -56,21 +60,31 @@ export default function Blog() {
             )}
           </Box>
           <Box>
-            <BodyTextLarge className={classes.textColor}></BodyTextLarge>
+            <BodyTextLarge color="grey">{blog.content}</BodyTextLarge>
           </Box>
           <Box mt={3}>
-            <BodyTextLarge
-              className={classes.textColor}
-              fontWeight="fontWeightMedium"
-            >
-              Alex Smith
+            <BodyTextLarge fontWeight="fontWeightMedium" color="grey">
+              {blog.shortText}
             </BodyTextLarge>
-            <BodyTextSmall className={classes.textColor}>
-              3 hours ago
-            </BodyTextSmall>
+            <BodyTextSmall color="grey">{blog.createdAt}</BodyTextSmall>
           </Box>
         </Box>
       </Box>
     </Box>
   );
 }
+
+Blog.propTypes = {
+  blog: PropTypes.object,
+};
+Blog.defaultProps = {
+  blog: {
+    title: '',
+    content: '',
+    shortText: '',
+    thumbnail: '',
+    createdAt: '',
+  },
+};
+
+export default memo(Blog);
