91e210b6d17b8709d8a04dd45d0f511d36c58656
fatimashah5184 <fatimashah5184@gmail.com>
2021-04-21 17:14:35 +0600

    divider added

--- app/components/blogListing/index.js
+++ app/components/blogListing/index.js
@@ -1,17 +1,9 @@
-import { Box } from '@material-ui/core';
+import { Box, Divider } from '@material-ui/core';
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Blog from '../pages/blog';
 import { H5 } from '../typography';
 
-const useStyles = makeStyles(() => ({
-  blog: {
-    borderBottom: '2px solid #eee',
-  },
-}));
-
 export function BlogListing(items) {
-  const classes = useStyles();
   return (
     <>
       <Box m={4}>
@@ -21,8 +13,9 @@ export function BlogListing(items) {
             {items &&
               items.blogs &&
               items.blogs.map((item) => (
-                <Box className={classes.blog}>
+                <Box>
                   <Blog item={item} />
+                  <Divider />
                 </Box>
               ))}
           </Box>
