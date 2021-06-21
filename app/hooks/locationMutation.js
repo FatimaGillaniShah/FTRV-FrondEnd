ee6601fa23cb3de87ff1b0611263b5ec903f695f
fatimashah5184 <fatimashah5184@gmail.com>
2021-06-21 13:40:22 +0600

    invalidate fix

--- app/hooks/locationMutation.js
+++ app/hooks/locationMutation.js
@@ -1,14 +1,17 @@
-import { useMutation } from 'react-query';
-import { Toast } from '../components';
+import { useMutation, useQueryClient } from 'react-query';
+import { Toast } from '../utils/helper';
 import { createLocation } from '../state/queryFunctions';
+import { keys } from '../state/queryKeys';
 
 export function useCreateLocation() {
+  const queryClient = useQueryClient();
   return useMutation(createLocation, {
     onSuccess: () => {
       Toast({
         icon: 'success',
         title: 'Location created successfully',
       });
+      queryClient.invalidateQueries(keys.location);
     },
     onError: ({
       response: {
