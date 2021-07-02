6b14ab3a8c38c28b23ab5d7e3e1cc308267ea185
fatimashah5184 <fatimashah5184@gmail.com>
2021-07-02 11:58:09 +0600

    drag and drop added in document list

--- app/components/pages/documents/documentList.js
+++ app/components/pages/documents/documentList.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Paper } from '@material-ui/core';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { Document } from './document';
 import { H5 } from '../../typography';
 import { useStyles } from './style';
@@ -10,7 +11,34 @@ export default function DocumentList({
   onHandleDelete,
 }) {
   const classes = useStyles();
+  const [departmentDocuments, updateDepartmentDocuments] = useState(documents);
 
+  const handleOnDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const items = Array.from(departmentDocuments);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    updateDepartmentDocuments(items);
+  };
+  const getStyle = (isDragging, draggableStyle) => {
+    const { transform } = draggableStyle;
+    let activeTransform = {};
+    if (transform) {
+      activeTransform = {
+        transform: `translate(0, ${transform.substring(
+          transform.indexOf(',') + 1,
+          transform.indexOf(')')
+        )})`,
+      };
+    }
+    return {
+      userSelect: 'none',
+      ...draggableStyle,
+      ...activeTransform,
+    };
+  };
   return (
     <Paper>
       <Box width={1}>
@@ -21,11 +49,42 @@ export default function DocumentList({
             </H5>
           </Box>
         </Paper>
-        <Box className={classes.documentList}>
-          {documents?.map((document) => (
-            <Document document={document} onHandleDelete={onHandleDelete} />
-          ))}
-        </Box>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="document">
+            {(provided) => (
+              <Box
+                className="document"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {departmentDocuments.map(({ id, name, description }, index) => (
+                  <Draggable key={id} draggableId={id} index={index}>
+                    {(providedDraggable, snapshot) => (
+                      <Box
+                        className={classes.documentList}
+                        ref={providedDraggable.innerRef}
+                        {...providedDraggable.draggableProps}
+                        {...providedDraggable.dragHandleProps}
+                        style={getStyle(
+                          snapshot.isDragging,
+                          providedDraggable.draggableProps.style
+                        )}
+                      >
+                        <Document
+                          document={document}
+                          onHandleDelete={onHandleDelete}
+                          name={name}
+                          description={description}
+                        />
+                      </Box>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </Box>
+            )}
+          </Droppable>
+        </DragDropContext>
       </Box>
     </Paper>
   );
