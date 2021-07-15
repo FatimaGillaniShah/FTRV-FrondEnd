d0b8f3080e5898e184c3e0d9f1a672f13914e85a
Hamza <ali.hamza@datics.ai>
2021-07-15 14:18:56 +0500

    moved design to pages

--- app/containers/ringGroup/index.js
+++ app/containers/ringGroup/index.js
@@ -1,22 +1,9 @@
 import React, { memo, useState } from 'react';
 import { Helmet } from 'react-helmet';
-import Box from '@material-ui/core/Box';
-import { Alert } from '@material-ui/lab';
-import { headCells } from '../../components/pages/ringGroup/columns';
-import WrapInCard from '../../components/layout/wrapInCard';
-import DataTable from '../../components/dataTable';
-import TableButtons from '../../components/pages/ringGroup/tableButtons';
-import { useAuthContext } from '../../context/authContext';
-import { ROLES } from '../../utils/constants';
-import WrapInBreadcrumbs from '../../components/layout/wrapInBreadcrumbs';
+import RingGroup from '../../components/pages/ringGroup';
 
 function RingGroupContainer() {
   const [selected, setSelected] = useState([]);
-  const {
-    user: {
-      data: { role },
-    },
-  } = useAuthContext();
   const data = [];
 
   return (
@@ -25,32 +12,7 @@ function RingGroupContainer() {
         <title> Ring Group</title>
       </Helmet>
 
-      <WrapInBreadcrumbs>
-        <Box width={1}>
-          <WrapInCard>
-            {role === ROLES.ADMIN && (
-              <Box mt={4}>
-                <TableButtons numSelected={selected.length} />
-                {selected.length > 0 && (
-                  <Box my={4}>
-                    <Alert severity="info">
-                      <strong>{selected.length}</strong> Ring Group(s) Selected
-                    </Alert>
-                  </Box>
-                )}
-              </Box>
-            )}
-            <DataTable
-              data={data}
-              headCells={headCells}
-              setSelected={setSelected}
-              selected={selected}
-              count={data?.length || 0}
-              sortColumn="name"
-            />
-          </WrapInCard>
-        </Box>
-      </WrapInBreadcrumbs>
+      <RingGroup data={data} selected={selected} setSelected={setSelected} />
     </>
   );
 }
