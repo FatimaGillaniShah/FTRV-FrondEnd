f59f520653f8ad0eddd7bd43366097bccadb5dfc
Hamza <ali.hamza@datics.ai>
2021-09-23 18:09:20 +0500

    removed useless anonymous function

--- app/containers/directory/columns.js
+++ app/containers/directory/columns.js
@@ -35,7 +35,7 @@ const ActionButtons = ({ data }) => {
       >
         <EditIcon color="secondary" />
       </IconButton>
-      <IconButton onClick={() => handleDeleteUser()} disabled={disabled}>
+      <IconButton onClick={handleDeleteUser} disabled={disabled}>
         <DeleteIcon color="error" />
       </IconButton>
     </>
